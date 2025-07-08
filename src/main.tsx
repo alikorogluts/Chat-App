d47779fe79cbafe09a9ce70bd29a6f9f0bfaa99e
AliKoroglu <124581897+alikorogluts@users.noreply.github.com>
2025-07-08 16:25:20 +0300

    canlıda hata çıkarması üzerine hashrooter kullınımına başlandı

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter basename="/Chat-App/">
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
