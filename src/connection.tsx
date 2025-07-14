0a2adfb54680db79b027899ca5987c051737a9c1
Ali Koroglu <220502006@ogrenci.artvin.edu.tr>
2025-07-14 17:42:09 +0300

    dosya gönderme hataları düzeltme v2

--- src/connection.tsx
+++ src/connection.tsx
@@ -2,7 +2,7 @@ interface conncectionApi {
     connectionString: string
 }
 export const apiConfig: conncectionApi = {
-    connectionString: "https://localhost:7223/"
-    //connectionString: "https://chat-app1.runasp.net/"
+    //connectionString: "https://localhost:7223/"
+    connectionString: "https://chat-app1.runasp.net/"
 
 };
